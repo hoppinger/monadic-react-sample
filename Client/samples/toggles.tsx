90613109ff1556f58dddfd0b29b182ffb6df0aef
giuseppemag <giuseppemag@gmail.com>
2017-07-21 10:06:33 +0200

    Removed last toggle.

--- Client/samples/toggles.tsx
+++ Client/samples/toggles.tsx
@@ -15,7 +15,7 @@ export let toggles_sample : C<void> =
         bool("edit", "checkbox", `basic toggle`)(b)),
       label<boolean, boolean>("my fancy toggle.")(b =>
         bool("edit", "fancy toggle", `fancy toggle`)(b)),
-      label<boolean, boolean>("The last toggle: ")(b =>
-        bool("edit", "plus/minus", `a plus/minus toggle.`)(b)),
+      // label<boolean, boolean>("The last toggle: ")(b =>
+      //   bool("edit", "plus/minus", `a plus/minus toggle.`)(b)),
     ])(b))(true).then(`fancy_toggle_bind`, c =>
   string("view")(`Your selection is ${c.toString()}`).ignore())
