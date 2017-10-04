e67b84881f4f4e0058c3f1eafb5c0fb8e2519171
Ronald Dijks <ronalddijks@protonmail.com>
2017-10-04 17:08:24 +0200

    Reenable routes

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use("/", express.static('wwwroot', {
   }
 }))
 
-// app.get('/css/site.css', function(req,res){
-//   res.sendFile(__dirname + '/css/site.min.css')
-// })
+app.get('/css/site.css', function(req,res){
+  res.sendFile(__dirname + '/css/site.min.css')
+})
 
-// app.get('/js/site.js', function(req,res){
-//   res.sendFile(__dirname + '/js/site.js')
-// })
+app.get('/js/site.js', function(req,res){
+  res.sendFile(__dirname + '/js/site.js')
+})
 
 app.get('*', function (req, res) {
   let slug = req.params[0] || ""
